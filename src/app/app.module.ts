e2da88260a666231dff240355cb107288135e56b
Coperfield <coperfield@hotmail.com>
2023-09-23 13:37:30 -0600

    mock_navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SpinnerModule } from './components/spinner/spinner.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [ AppComponent  ],
+  declarations: [ AppComponent, NavbarComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
