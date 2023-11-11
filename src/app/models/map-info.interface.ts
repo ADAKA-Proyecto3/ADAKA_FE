8d9ccdbda9bd6ec412d0711b9b4c16967448e7bc
Alberto Solano <70608100+AlbertoSolanoV@users.noreply.github.com>
2023-11-11 15:08:57 -0600

    Map feature

--- src/app/models/map-info.interface.ts
+++ src/app/models/map-info.interface.ts
@@ -4,4 +4,5 @@ export interface MapInfo {
     latitude: string;
     longitude: string;
     status: string;
+    valor: string;
 }
--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -10,6 +10,12 @@ import { MapInfoHttpService } from 'src/app/services/http-service/mapInfo-http.s
 import { loadMapInfo } from 'src/app/store/actions/mapInfo.actions';
 import { AppState } from 'src/app/store/app.state';
 
+export interface InfoData {
+  range: string;
+  level: string;
+  healthImplications: string;
+  cautionaryStatement: string;
+}
 
 @Component({
   selector: 'app-map',
@@ -21,22 +27,26 @@ export class MapPage implements AfterViewInit {
 
   private MapInfoList: MapInfo[] = [];
 
-
   map: L.Map | null = null;
   private view = {
     center: L.latLng(9.9202, -84.1031),
-    zoom: 9
+    zoom: 9,
   };
 
-  constructor(private store: Store<AppState>, private mapInfoService: MapInfoHttpService) {}
+  constructor(
+    private store: Store<AppState>,
+    private mapInfoService: MapInfoHttpService
+  ) {}
 
-  displayedColumns: string[] = [
-    'name',
-    'direction',
-    'status',
-    'actions',
-  ];
+  displayedColumns: string[] = ['name', 'direction', 'status', 'actions'];
   dataSource = new MatTableDataSource<MapInfo>();
+  dataSourceInfo = new MatTableDataSource<InfoData>();
+  displayedColumnsInfo: string[] = [
+    'range',
+    'level',
+    'healthImplications',
+    'cautionaryStatement',
+  ];
 
   ngOnInit(): void {
     this.getMapInfoaqicn();
@@ -46,31 +56,79 @@ export class MapPage implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.store.select('mapInfo').subscribe(({ mapInfo }) => {
-      const listaCombinada =  this.MapInfoList.concat(mapInfo);
-        this.MapInfoList = listaCombinada;
+      const listaCombinada = this.MapInfoList.concat(mapInfo);
+      this.MapInfoList = listaCombinada;
       this.dataSource.data = this.MapInfoList;
-      
     });
     this.dataSource.paginator = this.paginator;
+    this.initializeInfoData();
   }
+
+  private initializeInfoData(): void {
+    console.log()
+    this.dataSourceInfo = new MatTableDataSource<InfoData>([
+      {
+        range: '0 - 50',
+        level: 'Bueno',
+        healthImplications:
+          'La calidad del aire se considera satisfactoria y la contaminación atmosférica presenta poco o ningún riesgo.',
+        cautionaryStatement: 'Ninguna',
+      },
+      {
+        range: '51 - 100',
+        level: 'Moderado',
+        healthImplications:
+          'La calidad del aire es aceptable; sin embargo, para algunos contaminantes puede haber una preocupación moderada por la salud para un número muy pequeño de personas que son inusualmente sensibles a la contaminación atmosférica.',
+        cautionaryStatement:
+          'Los niños y adultos activos, y las personas con enfermedades respiratorias como el asma, deben limitar la actividad al aire libre prolongada.',
+      },
+      {
+        range: '101-150',
+        level: 'No saludable para grupos sensibles',
+        healthImplications:
+          'Los miembros de grupos sensibles pueden experimentar efectos en la salud. Es poco probable que el público en general se vea afectado.',
+        cautionaryStatement:
+          'Los niños y adultos activos, y las personas con enfermedades respiratorias como el asma, deben limitar la actividad al aire libre prolongada.',
+      },
+      {
+        range: '151-200',
+        level: 'No saludable',
+        healthImplications:
+          'Todos pueden comenzar a experimentar efectos en la salud; los miembros de grupos sensibles pueden experimentar efectos más graves en la salud.',
+        cautionaryStatement:
+          'Los niños y adultos activos, y las personas con enfermedades respiratorias como el asma, deben evitar la actividad al aire libre prolongada; todos los demás, especialmente los niños, deben limitar la actividad al aire libre prolongada.',
+      },
+      {
+        range: '201-300',
+        level: 'Muy no saludable',
+        healthImplications:
+          'Advertencias de salud por condiciones de emergencia. Es más probable que toda la población se vea afectada.',
+        cautionaryStatement:
+          'Los niños y adultos activos, y las personas con enfermedades respiratorias como el asma, deben evitar toda actividad al aire libre; todos los demás, especialmente los niños, deben limitar la actividad al aire libre.',
+      },
+      {
+        range: '300+',
+        level: 'Peligroso',
+        healthImplications:
+          'Alerta de salud: todos pueden experimentar efectos más graves en la salud',
+        cautionaryStatement: 'Todos deben evitar toda actividad al aire libre.',
+      },
+    ]);
+  }
+
   private loadInfo() {
     this.store
       .select('mapInfo')
       .pipe(
-        filter(
-          (mapInfo) =>
-          mapInfo.status === "success"
-        ),
+        filter((mapInfo) => mapInfo.status === 'success'),
         take(1)
       )
       .subscribe((mapInfo) => {
-
         this.createMap();
-        
       });
   }
 
-  private createMap(){
+  private createMap() {
     this.map = L.map('map').setView(this.view.center, this.view.zoom);
 
     const tileLayerOptions = {
@@ -87,63 +145,63 @@ export class MapPage implements AfterViewInit {
     ).addTo(this.map);
 
     this.addZone(this.MapInfoList);
-    
   }
 
   cambiarVista(mapInfo: MapInfo) {
-  
-    if (this.map) { // Asegúrate de que el mapa esté inicializado
-      this.map.setView([Number(mapInfo.latitude), Number(mapInfo.longitude)], 15);
+    if (this.map) {
+      // Asegúrate de que el mapa esté inicializado
+      this.map.setView(
+        [Number(mapInfo.latitude), Number(mapInfo.longitude)],
+        17
+      );
     }
   }
 
-  private addZone(listInfo: MapInfo[]){
-    listInfo.forEach(element => {
+  private addZone(listInfo: MapInfo[]) {
+    listInfo.forEach((element) => {
       if (this.map) {
-
         let color: string;
 
-        if (element.status === 'Saludable') {
+        if (element.status === 'Bueno') {
           color = 'green';
-        } else if (element.status === 'Moderada') {
+        } else if (element.status === 'Moderado') {
           color = 'yellow';
-        } else if (element.status === 'No saludable para ciertos grupos') {
+        } else if (element.status === 'No saludable para grupos sensibles') {
           color = 'orange';
         } else if (element.status === 'No saludable') {
           color = 'red';
-        } else if (element.status === 'Muy poco saludable') {
+        } else if (element.status === 'No muy saludable') {
           color = 'purple';
         } else {
           color = 'brown';
         }
 
         L.circle([Number(element.latitude), Number(element.longitude)], {
-          radius: 250,
+          radius: 50,
           color: color,
         })
-        .addTo(this.map)
-        .bindPopup(element.name);
+          .addTo(this.map)
+          .bindPopup("Rango: " +element.valor);
       }
     });
   }
 
   getMapInfoaqicn() {
     this.mapInfoService.getMapInfoaqicn().subscribe(
-      resp => {
-       
-        const mapInfoListAqicn: MapInfo[] = resp.map(item => {
+      (resp) => {
+        const mapInfoListAqicn: MapInfo[] = resp.map((item) => {
           let status: string;
 
           if (item.aqi >= 0 && item.aqi <= 50) {
-            status = 'Saludable';
+            status = 'Bueno';
           } else if (item.aqi <= 100) {
-            status = 'Moderada';
+            status = 'Moderado';
           } else if (item.aqi <= 150) {
-            status = 'No saludable para ciertos grupos';
+            status = 'No saludable para grupos sensibles';
           } else if (item.aqi <= 200) {
             status = 'No saludable';
           } else if (item.aqi <= 300) {
-            status = 'Muy poco saludable';
+            status = 'No muy saludable';
           } else {
             status = 'Peligrosos';
           }
@@ -154,18 +212,36 @@ export class MapPage implements AfterViewInit {
             latitude: item.lat.toString(),
             longitude: item.lon.toString(),
             status: status,
+            valor: item.aqi
           };
         });
 
-        const listaCombinada =  this.MapInfoList.concat(mapInfoListAqicn);
+        const listaCombinada = this.MapInfoList.concat(mapInfoListAqicn);
         this.MapInfoList = listaCombinada;
         this.dataSource.data = this.MapInfoList;
         this.dataSource.paginator = this.paginator;
-        this.addZone( this.MapInfoList);
+        this.addZone(this.MapInfoList);
       },
-      error => {
+      (error) => {
         console.error('Error al obtener datos de getMapInfoaqicn:', error);
       }
     );
+  }
+
+  getColorClass(status: string): string {
+    switch (status) {
+      case 'Bueno':
+        return 'green';
+      case 'Moderado':
+        return 'yellow';
+      case 'No saludable para grupos sensibles':
+        return 'orange';
+      case 'No saludable':
+        return 'red';
+      case 'No muy saludable':
+        return 'purple';
+      default:
+        return 'brown';
+    }
   }
 }
